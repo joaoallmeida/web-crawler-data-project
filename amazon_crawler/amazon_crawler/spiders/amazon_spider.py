3fad4339a55218082e2977cc1aa80e010d0f1191
joao soares <jaumalmeida@outlook.com>
2023-04-04 23:48:49 -0300

    Updated spider amazon

--- amazon_crawler/amazon_crawler/spiders/amazon_spider.py
+++ amazon_crawler/amazon_crawler/spiders/amazon_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from urllib.parse import urljoin
 
 class AmazonSpider(scrapy.Spider):
     name='amazon'
@@ -6,26 +7,33 @@ class AmazonSpider(scrapy.Spider):
 
     def parse(self, response):
         best_sellears_page = response.xpath('//*[@id="nav-xshop"]/a[2]')
-        for a in best_sellears_page:
-            yield response.follow(a,self.parse_best_sellears)
+        for page in best_sellears_page:
+            yield response.follow(page,self.parse_next_tab)
         
-    def parse_best_sellears(self, response):
-        for data in response.css('.a-inline-block .a-link-normal'):
-            product_details = data.css('.a-link-normal::attr(href)').get()
-            if product_details is not None:
-                yield response.follow(product_details,self.parse_product_details)
+    def parse_next_tab(self,response):
+        for tab in response.xpath('//*[@id="zg_header"]/div//ul//a'):
+            yield response.follow(tab, self.parse_department)
+
+    def parse_department(self, response):
+        for link in response.xpath("//div[@role='group']/div/a"):
+            product_page = link.css('a::attr(href)').get()
+            if product_page is not None:
+                yield response.follow(product_page, self.parse_product_details)
     
     def parse_product_details(self, response):
-
-        title = response.css('h1::text').get()
+        
+        h1 = response.css('h1::text').get()
+        depatment = h1.split('em ')[-1]
+        type = h1.split(' em')[0]
 
         for detail in response.css('div#gridItemRoot'):
             yield {
-                "title": title,
+                "type": type,
+                "department": depatment,
                 "rank": detail.css('span.zg-bdg-text::text').get(),
                 "product": detail.css('div::text').get(),
                 "rating_stars": detail.css('span.a-icon-alt::text').get(),
-                "rating": detail.css('span.a-size-small::text').get(),
+                "ratings": detail.css('span.a-size-small::text').get(),
                 "price": str(detail.css('span::text').getall()[-1]).replace('\xa0','')
             }
 
